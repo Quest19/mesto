e50a3b1a1c211b798310e32364a1c8b85b0b5e92
Renat Khuzin <79320329+Quest19@users.noreply.github.com>
2023-04-18 03:11:32 +0300

    pr 9 first validate

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   mode: 'development',
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'dist'),
+      directory: path.resolve(__dirname, './dist'),
     },
     open: true,
   },
